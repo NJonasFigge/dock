8c387d8a54542feb982b6b55f40ae981a0bf154d
jonasfigge <n.j.figge@gmail.com>
2025-09-30 15:46:28 +0200

    Developing

--- browse_containers.py
+++ browse_containers.py
@@ -62,7 +62,9 @@ class Browser:
         while self._log_stream_process is not None and self._log_stream_process.stdout is not None:
             for line in self._log_stream_process.stdout:
                 if ("fatal" in line.lower() or "error" in line.lower() or "can't" in line.lower()
-                        or "failed" in line.lower() or "no such" in line.lower() or "denied" in line.lower()):
+                        or "failed" in line.lower() or "no such" in line.lower() or "denied" in line.lower()
+                        or "unavailable" in line.lower() or "unreachable" in line.lower()
+                        or "not found" in line.lower()):
                     print(ANSICODES.RED_FG + line.strip() + ANSICODES.RESET, end='\n\r')
                 elif "warn" in line.lower():
                     print(ANSICODES.YELLOW_FG + line.strip() + ANSICODES.RESET, end='\n\r')
