ce320389a4e397b03e305954e5734a83fc0d42e7
jonasfigge <n.j.figge@gmail.com>
2025-09-30 15:05:03 +0200

    Developing

--- browse_containers.py
+++ browse_containers.py
@@ -6,6 +6,7 @@ import subprocess
 import datetime as dt
 from time import sleep
 from pathlib import Path
+from threading import Thread
 
 
 def _get_keypress():
@@ -40,20 +41,26 @@ class Browser:
             print("No running containers found.")
             exit()
         self._current_index = 0
+        self._log_stream_process = None
 
     @property
     def _current_container(self): return self._containers[self._current_index]
+
+    def _stream_process_output(self):
+        while self._log_stream_process is not None and self._log_stream_process.stdout is not None:
+            for line in self._log_stream_process.stdout:
+                print(line.strip())
 
     def _rotate(self, backwards: bool = False):
         self._current_index = (self._current_index + (-1 if backwards else 1)) % len(self._containers)
 
     def _start_log_stream(self):
         yml = Path(__file__).parent / 'src/docker-compose.yml'
-        p = subprocess.Popen(["docker", "compose", "-f", str(yml), "logs", "-f", self._current_container.name],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        for line in p.stdout:
-            print(line.strip())
-        return p
+        self._log_stream_process = subprocess.Popen(["docker", "compose", "-f", str(yml), "logs", "-f",
+                                                     self._current_container.name, "--no-log-prefix"],
+                                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        thread = Thread(target=self._stream_process_output, daemon=True)
+        thread.start()
 
     def _open_shell(self):
         subprocess.run(["make", "shell", "SERVICE=" + self._current_container.name])
@@ -63,11 +70,13 @@ class Browser:
             print(f"\033[48;5;250m\033[38;5;0m\n=== Showing logs for: {self._current_container.name} ===\033[0m")
             print(f"\033[48;5;240mPress [Enter] to open shell in {self._current_container.name}.\033[0m")
             print("\033[48;5;240mPress [A] and [D] to rotate through logs, [q] to quit.\033[0m\n")
-            log_stream_process = self._start_log_stream()
+            self._start_log_stream()
             key = _get_keypress()
             if key == "q":
                 print("Exiting...")
-                log_stream_process.terminate()
+                if self._log_stream_process is not None:
+                    self._log_stream_process.terminate()
+                    self._log_stream_process = None
                 break
             elif key == "a":
                 self._rotate(backwards=True)
@@ -76,7 +85,9 @@ class Browser:
                 self._rotate()
                 print("Rotating to next container...")
             elif key == "\r":  # Enter key
-                log_stream_process.terminate()
+                if self._log_stream_process is not None:
+                    self._log_stream_process.terminate()
+                    self._log_stream_process = None
                 subprocess.run(["make", "shell", "SERVICE=" + self._current_container.name])  # Blocking call
             sleep(1)  # Give time for the user to see the exit message
 
